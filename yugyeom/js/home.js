8d7a8c3b938ae614bb1bd10884a3d1db0d94184d
dayo2n <0217dayun@naver.com>
2021-11-11 18:18:48 +0900

    make type for editor

--- yugyeom/js/home.js
+++ yugyeom/js/home.js
@@ -1,9 +1,17 @@
 $(document).ready(function () { 
     // 테이블 셀 클릭시 해당 게시글을 조회하는 뷰로 이동하는 부분
     $("#notice_board_title tr").click(function(e){
+        var editorType = "none";
+        var postType = "viewPost";
         var rowIdx = e.target.closest("tr").rowIndex;
         if(rowIdx !== 0){ // th가 인덱스 0이므로 게시글 인덱스는 1부터 시작
-            $(location).attr('href', "viewPost.html"); // 경로 바꿔야함
+            $(location).attr('href', "viewPost.html?editorType=" + editorType + "&postType=" + postType); // 경로 바꿔야함
         }
+    });
+
+    $("#btn-createNewPost").click(function(e){
+        var editorType = "newEditor";
+        var postType = "editPost";
+        $(location).attr('href', "postEditor.html?editorType=" + editorType + "&postType=" + postType);
     });
 });
